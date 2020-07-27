2f9caddc5a2702173824594a0578f579c3544e29
Brais P. Castro <braispcastro@gmail.com>
2020-07-27 22:54:06 +0200

    Successful twitch auth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyfirmata, time, json
+from twitch import twitch
 
 # Find board
 board = pyfirmata.Arduino('/dev/tty.usbmodem14101')
@@ -11,8 +12,12 @@ with open('secret.json') as f:
     client_id = secret['clientId']
     client_secret = secret['clientSecret']
 
+twitch_client = twitch(client_id, client_secret)
+access_token = twitch_client.authenticate()
+
 
 while True:
+    # Placeholder blink
     board.digital[13].write(1)
     time.sleep(1)
     board.digital[13].write(0)
