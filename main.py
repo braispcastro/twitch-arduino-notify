e863726d6286275f9150dc8e53047fef459a1d49
Brais <braispc@ext.inditex.com>
2020-07-28 23:06:33 +0200

    Get username from secret.json

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ def get_streams_online(user_id):
     online_list = twitch_client.get_streams(access_token, users_id)
     result = []
     for channel in online_list:
-        result.append(twitchdata.ToStreamData(channel))
+        stream = twitchdata.ToStreamData(channel)
+        if stream.type == 'live':
+            result.append(stream)
     return result
 
 
@@ -25,20 +27,22 @@ def get_streams_online(user_id):
 # Twitch secrets
 client_id = ''
 client_secret = ''
+twitch_username = ''
 with open('secret.json') as f:
     secret = json.load(f)
     client_id = secret['clientId']
     client_secret = secret['clientSecret']
+    twitch_username = secret['twitchUsername']
 
 twitch_client = twitch(client_id, client_secret)
 access_token = twitch_client.authenticate()
-user_id = get_twitch_user_id('frankylift')
+user_id = get_twitch_user_id(twitch_username)
 streams = get_streams_online(user_id)
 streams_last_update = time.localtime()
 
 
 while True:
-    # Wait 5 minutos
+    # Wait 5 minutes
     time.sleep(300)
     aux_streams = get_streams_online(user_id)
     
