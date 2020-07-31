e6058b190b19e44d966ea5d0862cfa2899fedd6e
Brais P. Castro <braispcastro@gmail.com>
2020-08-01 01:42:59 +0200

    Migration from pyfirmata to pymata4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pyfirmata, json, time, twitchdata
+import json, time, twitchdata
+from pymata4 import pymata4
 from twitch import twitch
 from datetime import datetime
 
@@ -24,17 +25,28 @@ def get_streams_online(user_id):
 
 def alert_new_stream(new_online):
     for x in range(len(new_online)):
-        board.digital[buzzer].write(1)
+        board.digital_write(BUZZER, 1)
         time.sleep(0.2)
-        board.digital[buzzer].write(0)
+        board.digital_write(BUZZER, 0)
         time.sleep(0.2)
+
+
+def write_log(message):
+    str_now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    print(f"[{str_now}] {message}")
+
 
 # Board objects
-buzzer = 2
-led = 4
+LED_BUILTIN = 13
+BUZZER = 2
+LED = 4
 
-# Find board
-board = pyfirmata.Arduino('/dev/tty.usbmodem14101')
+# Init board
+board = pymata4.Pymata4(com_port='/dev/tty.usbmodem14101')
+board.set_pin_mode_digital_output(LED)
+board.set_pin_mode_digital_output(BUZZER)
+board.set_pin_mode_digital_output(LED_BUILTIN)
+board.digital_write(LED_BUILTIN, 0)
 
 # Twitch secrets
 client_id = ''
@@ -54,27 +66,26 @@ streams = get_streams_online(user_id)
 
 # Data gathered
 alert_new_stream(['init', 'test', 'beep'])
-print('Starting loop...')
+write_log('Starting loop...')
 
 while True:
     # Wait 60 seconds
     time.sleep(60)
 
     # Led ON
-    board.digital[led].write(1)
+    board.digital_write(LED, 1)
 
     # Get channels online
     aux_streams = get_streams_online(user_id)
     new_online = set(aux_streams).difference(streams)
     streams = aux_streams
-    str_now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-
+    
     # Board logic
     if len(new_online) > 0:
-        print(f"[{str_now}] {new_online}")
+        write_log(new_online)
         alert_new_stream(new_online)
     else:
-        print(f"[{str_now}] None")
+        write_log('None')
 
     # Led OFF
-    board.digital[led].write(0)
+    board.digital_write(LED, 0)
